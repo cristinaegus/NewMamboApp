e9d0f99c1020088b70d2ae2b2fda7369e86be5e2
unknown <egusquizacristina@gmail.com>
2025-05-12 12:09:13 +0200

    doy formato al formulario

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,7 @@ import react from '@astrojs/react';
 
 export default defineConfig({
   integrations: [tailwind(), react()],
+  devOptions: {
+    toolbar: false, // Desactiva la DevToolbar
+  },
 });
