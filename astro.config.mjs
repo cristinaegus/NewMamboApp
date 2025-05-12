709e9655abe19c4af7744f1a2270d505aa5eaf5c
unknown <egusquizacristina@gmail.com>
2025-05-12 14:49:34 +0200

    error con Tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import react from '@astrojs/react';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
+import tailwindcss from "tailwindcss/tailwind.css";
 
 export default defineConfig({
   integrations: [tailwind(), react()],
--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -7,33 +7,48 @@ const Formulario = () => {
     instrucciones: "",
   });
 
-  const handleSubmit = async (event) => {
+  const handleFormSubmit = async (event) => {
     event.preventDefault();
-    const response = await fetch("/api/guardar-receta", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(formData),
-    });
 
-    if (response.ok) {
-      alert("Receta guardada con éxito");
-      setFormData({ nombre: "", ingredientes: "", instrucciones: "" });
-    } else {
-      alert("Error al guardar la receta");
+    const form = event.target;
+    const formData = {
+      nombre: form.elements.namedItem("nombre")?.value || "",
+      ingredientes: form.elements.namedItem("ingredientes")?.value || "",
+      instrucciones: form.elements.namedItem("instrucciones")?.value || "",
+    };
+
+    try {
+      const response = await fetch("/api/guardar-receta", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (response.ok) {
+        alert("Receta guardada con éxito");
+        form.reset();
+        // fetchRecetas(); // Actualiza la lista de recetas
+      } else {
+        alert("Error al guardar la receta");
+      }
+    } catch (error) {
+      console.error("Error al enviar los datos:", error);
     }
   };
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleFormSubmit}>
       <input
         type="text"
+        name="nombre"
         placeholder="Nombre"
         value={formData.nombre}
         onChange={(e) => setFormData({ ...formData, nombre: e.target.value })}
       />
       <textarea
+        name="ingredientes"
         placeholder="Ingredientes"
         value={formData.ingredientes}
         onChange={(e) =>
@@ -41,6 +56,7 @@ const Formulario = () => {
         }
       />
       <textarea
+        name="instrucciones"
         placeholder="Instrucciones"
         value={formData.instrucciones}
         onChange={(e) =>
